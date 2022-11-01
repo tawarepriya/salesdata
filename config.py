949e2f3ceecc4828b4a3769bd8e70be4be80868d
Priya Taware <priya.taware01@gmail.com>
2022-11-01 22:33:17 +0530

    Sales Data Project Added

---
+++ config.py
@@ -0,0 +1,17 @@
+# from re import M
+
+
+# PORT_NUMBER = 5000
+# MODEL_PATH = "models\linear_model.pkl"
+# JSON_PATH = "models\project_data.json"
+
+import os
+
+
+CSV_FILE_PATH = "enter_path"
+
+PORT_NUMBER = 5000
+
+MODEL_FILE_PATH = os.path.join("model", "linear_model.pkl")
+
+JSON_FILE_PATH = os.path.join("model", "project_data.json")
